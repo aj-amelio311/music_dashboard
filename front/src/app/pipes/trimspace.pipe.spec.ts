329840f2267def5275f7545c6342a86159dd1302
AJ Amelio <aj.amelio90@gmail.com>
2019-01-28 15:48:31 -0500

    Delete trimspace.pipe.spec.ts

--- front/src/app/pipes/trimspace.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { TrimspacePipe } from './trimspace.pipe';
-
-describe('TrimspacePipe', () => {
-  it('create an instance', () => {
-    const pipe = new TrimspacePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
